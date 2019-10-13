5515921d0db0f94a519cccf98b330c51f2e4ecbd
purvimalik18 <purvi.malik_cs17@gla.ac.in>
2019-10-13 18:59:20 +0530

    commited

--- notepad.py
+++ notepad.py
@@ -5,23 +5,25 @@ menu = Menu(m)
 m.config(menu=menu)
 f = Menu(menu)
 
-menu.__Filemenu.add_command(label='File', menu='f')
-f.__Filemenu.add_cascade(label='New', command='f.__newFile')
-f.__Filemenu.add_cascade(label='Open', command='f.__openFile')
-f.__Filemenu.add_cascade(label='Save', command='f.__saveFile')
-f.__Filemenu.add_separator()
-f.__Filemenu.add_cascade(label='Exit', command=m.quit)
+menu.Filemenu.add_cascade(label='File', menu='f')
+f.Filemenu.add_command(label='New', command='f.__newFile')
+f.Filemenu.add_command(label='Open', command='f.__openFile')
+f.Filemenu.add_command(label='Save', command='f.__saveFile')
+f.Filemenu.add_separator()
+f.Filemenu.add_command(label='Exit', command=m.quit)
 
-menu.__Editmenu.add_command(label='Edit', menu='f')
-f.__Editmenu.add_cascade(label='Cut', command='f.__cut')
-f.__Editmenu.add_cascade(label='Copy', command='f.__copy')
-f.__Editmenu.add_cascade(label='Paste', command='f.__paste')
+menu.Editmenu.add_cascade(label='Edit', menu='f')
+f.Editmenu.add_command(label='Cut', command='f.__cut')
+f.Editmenu.add_command(label='Copy', command='f.__copy')
+f.Editmenu.add_command(label='Paste', command='f.__paste')
 
-menu.__Commandmenu.add_command(label='Commands', menu='f')
-f.__Commandmenu.add_command(label='About Commands', command='f.__showCommands')
+menu.Commandmenu.add_cascade(label='Commands', menu='f')
+f.Commandmenu.add_command(label='About Commands', command='f.__showCommands')
 
-menu.__Helpmenu.add_command(label='Help', menu='f')
-f.__Helpmenu.add_command(label='About Notepad')
+menu.Helpmenu.add_cascade(label='Help', menu='f')
+f.Helpmenu.add_command(label='About Notepad')
+
+
 mainloop()
 
 
